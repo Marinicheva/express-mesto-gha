a184d3e155fccd5d300a380484af6a0c8e679d1e
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-10-13 12:21:24 +0300

    refactor: use express.json, remove body-parser

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const userRouter = require('./routes/user');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb').then(() => console.log('Db is connected'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', userRouter);
 
