cc8a61a3a0f112b8263c0acb83662a9b699f8bdb
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-10-14 10:13:12 +0300

    feat: add update user and update avatar controllers

--- controllers/users.js
+++ controllers/users.js
@@ -26,12 +26,31 @@ const getUserById = (req, res) => {
 };
 
 const updateUser = (req, res) => {
-  const { id } = req.params;
   const { name, about } = req.body;
 
   return User.findByIdAndUpdate(
-    id,
+    req.user._id,
     { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  ).orFail(new Error('Not found')) // Посмотри в слак в чате группы пост от Жени какой там красивый текст ошибки
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.message === 'Not found') {
+        return res.status(404).send('Пользователь с таким id не найден');
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  return User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
     {
       new: true,
       runValidators: true,
@@ -51,4 +70,5 @@ module.exports = {
   createUser,
   getUserById,
   updateUser,
+  updateAvatar,
 };
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@ const {
   createUser,
   getUserById,
   updateUser,
+  updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.post('/', createUser);
 router.get('/:id', getUserById);
-router.patch('/:id', updateUser);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
