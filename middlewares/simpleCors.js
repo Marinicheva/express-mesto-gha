e338b10c3be426321b4ab3ca5cabffe21dc640b3
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-07 22:55:20 +0300

    feat: try the simple cors requests

--- middlewares/simpleCors.js
+++ middlewares/simpleCors.js
@@ -4,6 +4,7 @@ const simpleCors = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    next();
   } else {
     next();
   }
